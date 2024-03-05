58b49acf97cfe6554fbcc14f9f64188709ad8aff
lubiano83 <lubiano83@gmail.com>
2024-03-05 10:37:16 -0300

    arreglando contador

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,7 +1,7 @@
 /* Boton */
 
-const Boton = ({label, href}) => {
+const Boton = ({label, href, handleClick}) => {
   return (
-      <button className="boton" to={href} >{label}</button>
+      <button className="boton" to={href} onClick={handleClick}>{label}</button>
   )
 }; export default Boton;
