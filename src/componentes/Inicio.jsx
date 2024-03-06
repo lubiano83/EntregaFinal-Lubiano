a82aac637debda86fceef2073b9a4fe9ff84e6d5
lubiano83 <lubiano83@gmail.com>
2024-03-06 14:25:22 -0300

    agregando texto de inicio

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,5 +1,6 @@
 /* Inicio */
 
+import Bienvenida from "./Bienvenida";
 import Entrar from "./Entrar";
 import Tools from "./Tools";
 
@@ -8,6 +9,7 @@ const Inicio = () => {
     <div id="Inicio">
         <Tools />
         <Entrar />
+        <Bienvenida />
     </div>
   )
 }; export default Inicio;
