5a5292a23e88293c629b4279105417354d44a5ee
lubiano83 <lubiano83@gmail.com>
2024-03-07 15:04:56 -0300

    agregando seccion de ingreso

--- src/componentes/Ingreso.jsx
+++ src/componentes/Ingreso.jsx
@@ -1,9 +1,18 @@
 /* Ingreso */
 
+import Boton from "./Boton";
+
 const Ingreso = () => {
   return (
-    <section>
-        
+    <section className="mostrar-ingreso" id="Ingreso">
+      <h3 className="IngresoTexto">Ingresa tus Datos:</h3>
+      <div className="mb-3 column formulario">
+        <label htmlFor="imputEmail" className="col-sm-2 col-form-label">Email</label>
+        <input type="mail" className="form-control" id="inputMail"/>
+        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+        <input type="password" className="form-control" id="inputPassword"/>
+      </div>
+      <Boton label="Ingresa"/>
     </section>
   )
 }; export default Ingreso;
--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -3,12 +3,14 @@
 import InicioTexto from "./InicioTexto";
 import InicioBoton from "./InicioBoton";
 import InicioTools from "./InicioTools";
+import Ingreso from "./Ingreso";
 
 const Inicio = () => {
   return (
     <div id="Inicio">
         <InicioTools />
         <InicioBoton />
+        <Ingreso />
         <InicioTexto />
     </div>
   )
