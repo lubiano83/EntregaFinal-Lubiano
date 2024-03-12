83b8e65f35a56c85505fab249948335ad7195263
lubiano83 <lubiano83@gmail.com>
2024-03-12 18:36:31 -0300

    recibiendo datos del checkout en el mail

--- src/componentes/Checkout.jsx
+++ src/componentes/Checkout.jsx
@@ -5,41 +5,35 @@ import Titulo from "./Titulo";
 import { Link } from "react-router-dom";
 
 const Checkout = () => {
+
   return (
     <form id="Checkout" action="https://formsubmit.co/lubiano83@gmail.com" method="POST">
-        <Titulo label="Datos de Envio:"/>
-          <div className="barra">
-            <label htmlFor="">Nombre</label>
-            <input type="text" name="text" required placeholder="Coloca tu Nombre"/>
-          </div>
-          <div className="barra">
-            <label htmlFor="">Mail</label>
-            <input type="email" name="email" required placeholder="example@mail.com"/>
-          </div>
-          <div className="barra">
-            <label htmlFor="">Región</label>
-            <input type="text" name="text" required placeholder="Coloca tu Región"/>
-          </div>
-          <div className="barra">
-            <label htmlFor="">Comuna</label>
-            <input type="text" name="text" required placeholder="Coloca tu Comuna"/>
-          </div>
-          <div className="barra">
-            <label htmlFor="">Dirección</label>
-            <input type="text" name="text" required placeholder="Coloca tu Dirección"/>
-          </div>
-          <div className="barra">
-            <label htmlFor="">Telefono</label>
-            <input type="text" name="text" required placeholder="Coloca tu Telefono"/>
-          </div>
+      <Titulo label="Datos de Envio:"/>
+      <div className="barra">
+        <label htmlFor="">Nombre</label>
+        <input type="name" name="name" required placeholder="Coloca tu Nombre"/>
+      </div>
+      <div className="barra">
+        <label htmlFor="">Mail</label>
+        <input type="email" name="email" required placeholder="example@mail.com"/>
+      </div>
+      <div className="barra">
+        <label htmlFor="">Dirección</label>
+        <input type="address" name="address" required placeholder="Region, Comuna, Calle y Numero."/>
+      </div>
+      <div className="barra">
+        <label htmlFor="">Telefono</label>
+        <input type="fone" name="fone" required placeholder="Coloca tu Telefono"/>
+      </div>
 
-          <input type="hidden" name="_next" value="http://localhost:5173/"/>
-          <input type="hidden" name="_captcha" value="false"/>
+      <input type="hidden" name="_next" value="http://localhost:5173/"/>
+      <input type="hidden" name="_captcha" value="false"/>
 
-        <Link to={"/"}>
-          <Boton label="Volver"/>
-        </Link>
-        <Boton label="Pagar" type="submit" name="submit" />
+      <Link to={"/"}>
+        <Boton label="Volver"/>
+      </Link>
+      <Boton label="Pagar" type="submit" name="submit" />
     </form>
+
   )
 }; export default Checkout;
--- src/componentes/Ingreso.jsx
+++ src/componentes/Ingreso.jsx
@@ -1,24 +1,8 @@
 /* Ingreso */
 
 import Boton from "./Boton";
-import { useState, useEffect } from "react";
 
 const Ingreso = () => {
-
-  const [mostrarIngreso, setMostrarIngreso] = useState(true);
-
-    const TOGGLE_INGRESO = () => {
-        setMostrarIngreso(!mostrarIngreso);
-    };
-
-    useEffect(() => {
-        const BOTON_TOGGLE = document.getElementById("Ingreso");
-        if (mostrarIngreso) {
-          BOTON_TOGGLE.classList.add("mostrar-ingreso");
-        } else {
-          BOTON_TOGGLE.classList.remove("mostrar-ingreso");
-        }
-    }, [mostrarIngreso]);
   
   return (
     <section className="mostrar-ingreso" id="Ingreso">
@@ -29,8 +13,7 @@ const Ingreso = () => {
         <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
         <input type="password" className="form-control" id="inputPassword"/>
       </div>
-      <Boton label="Salir" handleClick={TOGGLE_INGRESO}/>
-      <Boton label="Ingresa" />
+      <Boton label="Ingresar" />
     </section>
   )
 }; export default Ingreso;
