f41ae42319349ded73f4b42d1341494a03249fa6
lubiano83 <lubiano83@gmail.com>
2024-03-16 14:54:30 -0300

    ajuste pequeño

--- src/componentes/CheckoutItem.jsx
+++ src/componentes/CheckoutItem.jsx
@@ -6,7 +6,7 @@ const CheckoutItem = ({id, cantidad}) => {
 
   return (
     <article id="CheckoutItem">
-        <input type={`producto${id}`} name={`producto${id}`} readOnly value={`Id: ${id}, Cantidad: ${cantidad}.`}/>
+        <input type={`producto(${id})`} name={`producto(${id})`} readOnly value={`Cantidad: ${cantidad}.`}/>
     </article>
   )
 }; export default CheckoutItem;
